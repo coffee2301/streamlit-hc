37beb12a27d8867a2f310c21aab0b8d10c4a417a
coffee2301 <123149540+coffee2301@users.noreply.github.com>
2023-01-20 17:02:52 +0900

    Update hyc04.py

--- hyc04.py
+++ hyc04.py
@@ -5,7 +5,7 @@ import numpy as np
 import streamlit as st
 
 st.title('Annual monthly rates of KPI')
-st.title('Monthly _rates_ :blue[2020-2022] of rates and price :sunglasses:')
+st.title('Monthly _rates_ of :blue[2020-2022] and price :sunglasses:')
 
 money = pd.read_csv("money_data7.csv")
 
