bdb0499fc67d7d07c7f8a7a325051fe4b19c27c2
coffee2301 <123149540+coffee2301@users.noreply.github.com>
2023-01-27 09:44:32 +0900

    Update hyc04.py

--- hyc04.py
+++ hyc04.py
@@ -11,7 +11,13 @@ st.title('Annual monthly rates of KPI')
 st.title('Monthly _interest, kospi rates_ of :blue[2020-2022] and house price :sunglasses:')
 
 def  plotting_demo():
-    money = pd.read_csv("money_data7.csv")
+    
+    uploaded_file = st.file_uploader("Choose a file")
+
+    money=pd.read_csv(uploaded_file)
+    
+#     money = pd.read_csv("money_data7.csv")
+
     option = st.selectbox(
         'How would you like to choice year ?',
         ('2020', '2021', '2022'))
@@ -56,4 +62,7 @@ with st.form(key ='Form1'):
         
         
 if select_language =='line graph':        
-    plotting_demo()
+    try:
+          plotting_demo()  
+    except:      
+          pass
